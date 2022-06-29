5453aee479883d173440aeb2937cf1d3e607178a
龙玺文 <longxiwen@tprut.com>
2022-06-29 11:49:44 +0800

    ✨ feat: 支持 TS 类型检查

--- src/main.d.ts
+++
@@ -1,5 +0,0 @@
-import type { add } from './math';
-import type { base64ToFile } from './file';
-import type { arrayFlat } from './array';
-
-export { add, base64ToFile, arrayFlat };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module '@evanlong/utils' {
-  import type * as Main from './main.d';
-  const main: Main;
-  export default main;
-}
