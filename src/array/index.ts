80bce600df63f63be2c2104cefed048ccdab2106
123木头人 <1206993746@qq.com>
2022-07-02 18:42:59 +0800

    ✨ feat: update

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,17 +1,29 @@
 /**
  * 数组递归扁平化
- * @param {Array} target - 源数组
- * @param {String} childName - 子数组键名
+ * @param {Array} target 源数组 - 必填
+ * @param {String} childName 子数组键名，默认为children - 非必填
+ * @param {Function} transform 回调函数，转换数据结构 - 非必填
  * @returns data - 扁平化的新数组
+ * @example
+ * arrayFlat(data, 'children', ({ id, name }, index) => ({
+ *   id,
+ *   name: name + '-转换',
+ *   index,
+ * }));
  */
 export const arrayFlat = (
-  target: Array<any>,
-  childName: string = 'children',
-  data: Array<any> = []
+  target: any[],
+  transform?: (item: any, index: number) => any,
+  childName: string = 'children'
 ) => {
+  let data: any[] = [];
   target.forEach((item) => {
     Object.keys(item).length && data.push(item);
-    item[childName]?.length && arrayFlat(item[childName], childName, data);
+    if (item[childName]) {
+      data = data.concat(arrayFlat(item[childName], transform, childName));
+    }
   });
-  return data.map(({ [childName]: name, ...item }) => item);
+  return transform
+    ? data.map((item, index) => transform(item, index))
+    : data.map(({ [childName]: children, ...item }) => item);
 };
